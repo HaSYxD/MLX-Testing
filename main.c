a448dda3dfc653286039ca00dae15c1068aedf0f
HaSYxD <anthonyliaudet@gmail.com>
2023-11-23 07:36:34 +0100

    Event handling testing

--- main.c
+++ main.c
@@ -1,21 +1,64 @@
-#include "basic_shape.h"
+#include <stdlib.h>
+#include <X11/keysym.h>
+#include <X11/X.h>
+#include "mlx.h"
+#include <stdio.h>
+
+typedef struct	s_img
+{
+	void	*mlx_img;
+	char	*addr;
+	int	bpp;
+	int	line_len;
+	int	endian;
+}	t_img;
+typedef struct	s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_img	img;
+}	t_data;
+
+void	put_pixel_to_image(t_img *img, int x, int y, int color)
+{
+	char	*pixel;
+
+	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	*(int *)pixel = color;
+}
+
+int	handle_no_event(void *data)
+{
+	return (0);
+}
+
+int	handle_input(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+
+	return (0);
+}
 
 int	main(void)
 {
-	void	*mlx;
-	t_data	plane;
-	void	*mlx_win;
+	t_data	data;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World!");
+	data.mlx_ptr = mlx_init();
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 1920, 1080, "Hello World!");
 	
-	plane.img = mlx_new_image(mlx, 1920, 1080);
-	plane.addr = mlx_get_data_addr(plane.img, &plane.bpp, &plane.line_length, &plane.endian);
-	
-	draw_rect(&plane, 5, 5, 100, 100, 0x00FF0000);
-	draw_circle(&plane, 500, 500, 250, 0x0000FF00);
-	mlx_put_image_to_window(mlx, mlx_win, plane.img, 0, 0);
+	data.img.mlx_img = mlx_new_image(data.mlx_ptr, 1920, 1080);
+	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp,
+			&data.img.line_len, &data.img.endian);
+	put_pixel_to_image(&data.img, 10, 10, 0x00FF0000);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img.mlx_img, 0, 0);
 
-	mlx_loop(mlx);
+	mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_input, &data);
+
+	mlx_loop(data.mlx_ptr);
+
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
 	return (0);
 }
